6511ec4fc80838155b787dea855bd681724c9331
Niraj Sanghvi <niraj.sanghvi@gmail.com>
2014-01-14 22:59:51 -0800

    Added additional JS Config slots
    Added slots and renamed local storage to unique names

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -6,17 +6,20 @@ Pebble.addEventListener("ready", function() {
 });
 
 Pebble.addEventListener("showConfiguration", function() {
-  var options = JSON.parse(window.localStorage.getItem("options"));
+  var saveOptions = JSON.parse(window.localStorage.getItem("options"));
 	
   var url = "http://niraj.com/pebble/polar_clock_config.php?v1.0";
 	
   if(options != null) {
-    url += "&time=" + (options["0"] ? encodeURIComponent(options["0"]) : "") + 
-           "&date=" + (options["1"] ? encodeURIComponent(options["1"]) : "") +
-		   "&rowd=" + (options["2"] ? encodeURIComponent(options["2"]) : "") +
-		   "&invt=" + (options["3"] ? encodeURIComponent(options["3"]) : "");
+    url += "&time=" + (saveOptions["polarclock0"] ? encodeURIComponent(saveOptions["polarclock0"]) : "") + 
+           "&date=" + (saveOptions["polarclock1"] ? encodeURIComponent(saveOptions["polarclock1"]) : "") +
+		   "&rowd=" + (saveOptions["polarclock2"] ? encodeURIComponent(saveOptions["polarclock2"]) : "") +
+		   "&invt=" + (saveOptions["polarclock3"] ? encodeURIComponent(saveOptions["polarclock3"]) : "") +
+		   "&four=" + (saveOptions["polarclock4"] ? encodeURIComponent(saveOptions["polarclock4"]) : "") +
+		   "&five=" + (saveOptions["polarclock5"] ? encodeURIComponent(saveOptions["polarclock5"]) : "") +
+		   "&six="  + (saveOptions["polarclock6"] ? encodeURIComponent(saveOptions["polarclock6"]) : "");
   }
-	console.log("showing configuration: " + url);
+  console.log("Showing configuration: " + url);
   Pebble.openURL(url);
 });
 
@@ -28,7 +31,17 @@ Pebble.addEventListener("webviewclosed", function(e) {
 			
 			console.log("Options = " + JSON.stringify(options));
 			
-			window.localStorage.setItem("options", JSON.stringify(options));
+			var saveOptions = {
+				"polarclock0": options[0],
+				"polarclock1": options[1],
+				"polarclock2": options[2],
+				"polarclock3": options[3],
+				"polarclock4": options[4],
+				"polarclock5": options[5],
+				"polarclock6": options[6]
+			}
+		
+			window.localStorage.setItem("options", JSON.stringify(saveOptions));
 			
 			Pebble.sendAppMessage(options,
 				function(e) {
